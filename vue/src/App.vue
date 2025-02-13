dd468ff2a82ce964f61c0118607012c5ced2565d
viktorkaa <83224403+viktorkaa@users.noreply.github.com>
2025-02-13 09:05:30 +0100

    CSS
    Login, Auth/Overlay Container, Registration formatting and designing (not final)

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -13,5 +13,27 @@ import AuthContainer from './components/AuthContainer.vue'
   </footer>
 </template>
 
+<style scoped>
+footer {
+  background-color: #222;
+  color: #fff;
+  font-size: 14px;
+  bottom: 0;
+  position: fixed;
+  left: 0;
+  right: 0;
+  text-align: center;
+  z-index: 999;
+}
+
+footer p {
+  margin: 10px 0;
+}
+
+footer a , i{
+  color: #3c97bf;
+  text-decoration: none;
+}
+</style>
 
 
--- vue/src/components/RegisterForm.vue
+++ vue/src/components/RegisterForm.vue
@@ -16,4 +16,35 @@
       </form>
     </div>
   </template>
+  <style scoped>
+  .sign-in-container {
+    left: 0;
+    width: 50%;
+    z-index: 2;
+  }
+  
+  .container.right-panel-active .sign-in-container {
+    transform: translateX(100%);
+  }
+  
+  form {
+    background-color: #FFFFFF;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 0 50px;
+    height: 100%;
+    text-align: center;
+  }
+  
+  input {
+    background-color: #eee;
+    border: none;
+    padding: 12px 15px;
+    margin: 8px 0;
+    width: 100%;
+  }
+
+  </style>
 
