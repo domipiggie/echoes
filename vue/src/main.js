b22f1bea77a1dcd51985e8a87fc8b6a3086ebf78
domipiggie <domipiggie@gmail.com>
2025-03-24 08:23:54 +0100

    Store accessToken and userID in cookie

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,8 +3,11 @@ import { createPinia } from 'pinia';
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import VueCookies from 'vue-cookies'
 
-const app = createApp(App);
-app.use(createPinia());
-app.use(router);
-app.mount("#app");
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.use(VueCookies, { expires: '7d' })
+
+app.mount('#app')
--- vue/src/store/UserdataStore.js
+++ vue/src/store/UserdataStore.js
@@ -2,22 +2,33 @@ import {defineStore} from 'pinia';
 import {ref} from 'vue';
 
 export const userdataStore = defineStore("userdata", ()=>{
-    const accessToken = ref("");
-    const userID = ref("");
+    const accessToken = ref($cookies.get('accessToken') || "");
+    const userID = ref($cookies.get('userID') || "");
 
     function setAccessToken(token){
         accessToken.value = token;
+        $cookies.set('accessToken', token);
     }
+    
     function getAccessToken(){
         return accessToken.value;
     }
 
     function setUserID(id) {
         userID.value = id;
+        $cookies.set('userID', id);
     }
+    
     function getUserID() {
         return userID.value;
     }
 
-    return {setAccessToken, getAccessToken, setUserID, getUserID};
+    function clearAuth() {
+        accessToken.value = "";
+        userID.value = "";
+        $cookies.remove('accessToken');
+        $cookies.remove('userID');
+    }
+
+    return {setAccessToken, getAccessToken, setUserID, getUserID, clearAuth};
 });
