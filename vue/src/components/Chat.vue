deb4d1e6dd173208ce36068209d47674655507da
domipiggie <domipiggie@gmail.com>
2025-05-09 07:58:25 +0200

    fix

--- vue/src/components/Chat.vue
+++ vue/src/components/Chat.vue
@@ -1,74 +1,52 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 import Sidebar from './SideBar.vue';
 import ChatWindow from './ChatWindow.vue';
 import { userdataStore } from '../store/UserdataStore';
-import { useWebSocket } from '../composables/useWebSocket';
 import { useFriendshipStore } from '../store/FriendshipStore';
 import { useChannelStore } from '../store/ChannelStore';
 import { useMessageStore } from '../store/MessageStore';
-import { messageFormatter } from '../utils/messageFormatter';
-import { chatService } from '../services/chatService';
-import { fileService } from '../services/fileService';
-import { WS_CONFIG } from '../config/ws';
+import { useWebSocketStore } from '../store/WebSocketStore';
+
+import { handleNewMessage, handleDeleteMessage, handleOnFriendAdded } from '../composables/websocketFunctions.js';
 
 const userStore = userdataStore();
 const friendshipStore = useFriendshipStore();
 const channelStore = useChannelStore();
 const messageStore = useMessageStore();
+const webSocketStore = useWebSocketStore();
 const showChat = ref(false);
 const isMobile = ref(false);
-
-const processedFriendRequests = ref(new Set());
-
-const handleWebSocketMessage = (data) => {
-  console.log('[WebSocket] New message received:', data);
-  
-  if (data.type === 'new_message') {
-    if (parseInt(data.message.userID) === parseInt(userStore.getUserID())) {
-      console.log('[WebSocket] Ignoring message from self');
-      return;
-    }
-    
-    if (messageStore.getCurrentChannelId == data.channelID) {
-      const formattedMessage = messageFormatter.formatIncomingMessage(data.message);
-      console.log('[WebSocket] Adding formatted message to chat:', formattedMessage);
-      messageStore.getMessages.push(formattedMessage);
-    }
-  } else if (data.type === 'friend_request') {
-    const friendshipID = data.friendRequest?.friendshipID;
-    
-    if (friendshipID && !processedFriendRequests.value.has(friendshipID)) {
-      console.log('[WebSocket] Dispatching friend request event for ID:', friendshipID);
-      processedFriendRequests.value.add(friendshipID);
-      
-      window.dispatchEvent(new CustomEvent('new-friend-request', { 
-        detail: data 
-      }));
-    } else {
-      console.log('[WebSocket] Skipping duplicate friend request dispatch for ID:', friendshipID);
-    }
-  }
-};
-
-const { connectWebSocket } = useWebSocket(handleWebSocketMessage);
 
 onMounted(async () => {
   checkScreenSize();
   window.addEventListener('resize', checkScreenSize);
-  
+
   try {
     await friendshipStore.fetchFriendships();
     await channelStore.fetchAllChannels();
 
     console.log('Channels loaded:', channelStore.getFriendChannels, channelStore.getFriendChannels);
     console.log('Friendships loaded:', friendshipStore.getFriendships);
+
+    if (userStore.getAccessToken() != null) {
+      webSocketStore.connect();
+
+      webSocketStore.registerHandler('new_message', handleNewMessage);
+      webSocketStore.registerHandler('message_deleted', handleDeleteMessage);
+      webSocketStore.registerHandler('friend_add', handleOnFriendAdded);
+    }
   } catch (error) {
     console.error('Failed to load channels:', error);
   }
-  console.log('[Chat] Attempting to connect to WebSocket server at:', WS_CONFIG.BASE_URL);
-  connectWebSocket();
-  console.log('[Chat] WebSocket connection initiated');
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', checkScreenSize);
+
+  webSocketStore.unregisterHandler('new_message');
+  webSocketStore.unregisterHandler('friend_request');
+  webSocketStore.disconnect();
 });
 
 const checkScreenSize = () => {
@@ -77,13 +55,13 @@ const checkScreenSize = () => {
 
 const handleChatSelect = async (chat) => {
   messageStore.setCurrentChannelId(chat.getChannelID());
-  
-  var name = userStore.getUserID() == chat.getUser1().getUserID() 
-    ? chat.getUser2().getUserName() 
+
+  var name = userStore.getUserID() == chat.getUser1().getUserID()
+    ? chat.getUser2().getUserName()
     : chat.getUser1().getUserName();
-  
+
   messageStore.setCurrentChannelName(name);
-  
+
   try {
     await messageStore.fetchMessages();
 
@@ -91,7 +69,7 @@ const handleChatSelect = async (chat) => {
   } catch (error) {
     console.error('Error fetching messages:', error);
   }
-  
+
   if (isMobile.value) {
     showChat.value = true;
   }
@@ -102,65 +80,49 @@ const goBackToList = () => {
 };
 
 const sendMessage = async (text) => {
-  const newId = messageStore.getMessages.length + 1;
-  let messageContent;
-  let messageType = 'text';
-  let replyToId = null;
-  
-  const messageObj = messageFormatter.formatOutgoingMessage(text, newId);
-  // Hozzáadjuk az üzenetet a store-hoz
-  messageStore.getMessages.push(messageObj);
-  
-  if (typeof text === 'object') {
-    messageContent = text.text;
-    messageType = text.type || 'text';
-    
-    // Ha válasz üzenet, akkor elmentjük a válaszolt üzenet ID-jét
-    if (text.replyTo) {
-      replyToId = text.replyTo;
-      console.log('Válasz üzenet küldése, válaszolt üzenet ID:', replyToId);
-    }
-  } else {
-    messageContent = text;
-  }
-  
   try {
-    const response = await chatService.sendMessage(
-      messageStore.getCurrentChannelId,
-      messageContent,
-      messageType,
-      replyToId // Továbbítjuk a válaszolt üzenet ID-jét
-    );
-    
-    console.log('Message sent successfully:', response);
-    
-    if (response && response.messageID) {
-      // Frissítjük az üzenet ID-t a válasz alapján
-      const msgIndex = messageStore.getMessages.findIndex(msg => msg.id === messageObj.id);
-      if (msgIndex !== -1) {
-        messageStore.getMessages[msgIndex].id = response.messageID;
-      }
+    if (webSocketStore.getIsConnected) {
+      console.log('Sending message:', text);
+      webSocketStore.send({
+        type: 'chatmessage_send',
+        channelId: messageStore.getCurrentChannelId,
+        content: text.text,
+        messageType: text.type
+      });
     }
   } catch (error) {
     console.error('Error sending message:', error);
   }
 };
 
+const deleteMessage = async (messageId) => {
+  try {
+    if (webSocketStore.getIsConnected) {
+      webSocketStore.send({
+        type: 'chatmessage_delete',
+        channelId: messageStore.getCurrentChannelId,
+        messageId: messageId
+      });
+    }
+  } catch (error) {
+    console.error('Error deleting message:', error);
+  }
+}
 
 const updateMessage = async (updatedMessage) => {
   console.log('Üzenet frissítés fogadva:', updatedMessage);
-  
-  const messageIndex = messageStore.getMessages.findIndex(msg => msg.id === updatedMessage.id);
-  
+
+  const messageIndex = messages.value.findIndex(msg => msg.id === updatedMessage.id);
+
   if (messageIndex !== -1) {
-    messageStore.getMessages[messageIndex] = updatedMessage;
-    
+    messages.value[messageIndex] = updatedMessage;
+
     try {
       if (updatedMessage.isRevoked) {
-        await chatService.deleteMessage(messageStore.getCurrentChannelId, updatedMessage.id);
+        await chatService.deleteMessage(currentChat.value.channelID, updatedMessage.id);
       } else {
         await chatService.updateMessage(
-          messageStore.getCurrentChannelId,
+          currentChat.value.channelID,
           updatedMessage.id,
           updatedMessage.text
         );
@@ -174,20 +136,7 @@ const updateMessage = async (updatedMessage) => {
 
 const handleFileUpload = async (fileData) => {
   try {
-    console.log('Uploading file:', fileData.file.name);
-    const response = await fileService.uploadFile(fileData.file, fileData.channelID);
-    console.log('File uploaded successfully:', response);
-    
-    if (response && response.messageID) {
-      const tempMessageIndex = messageStore.getMessages.findIndex(
-        msg => msg.fileName === fileData.file.name && msg.sender === 'me'
-      );
-      
-      if (tempMessageIndex !== -1) {
-        messageStore.getMessages[tempMessageIndex].id = response.messageID;
-        console.log('Updated message with server ID:', response.messageID);
-      }
-    }
+
   } catch (error) {
     console.error('Error uploading file:', error);
   }
@@ -196,17 +145,9 @@ const handleFileUpload = async (fileData) => {
 
 <template>
   <div class="chat-application">
-    <Sidebar 
-      v-if="!isMobile || !showChat" 
-      @select-chat="handleChatSelect" 
-    />
-    <ChatWindow
-      v-if="!isMobile || showChat"
-      @send-message="sendMessage"
-      @send-file="handleFileUpload"
-      @go-back="goBackToList"
-      @update-message="updateMessage"
-    />
+    <Sidebar v-if="!isMobile || !showChat" @select-chat="handleChatSelect" />
+    <ChatWindow v-if="!isMobile || showChat" @send-message="sendMessage" @send-file="handleFileUpload"
+      @go-back="goBackToList" @update-message="updateMessage" @delete-message="deleteMessage" />
   </div>
 </template>
 
